b3beb4c87bdb3780fb3588fe18406688e5fb05ba
tymon <m.tymczarnocki@gmail.com>
2019-05-05 21:00:42 +0200

    schema adjusted, keys generated properly

--- repository/structs.go
+++ repository/structs.go
@@ -25,10 +25,10 @@ type EventTypeImplementation struct {
 	PreciseUse string
 	Status     ImplementationStatus
 
-	EventTypeId int64 `sql:"on_delete:RESTRICT, on_update: CASCADE"`
+	EventTypeId int64 `sql:"on_delete:RESTRICT, on_update: CASCADE, pk"`
 	EventType   *EventType
 
-	SourceSystemId int32 `sql:"on_delete:RESTRICT, on_update: CASCADE"`
+	SourceSystemId int32 `sql:"on_delete:RESTRICT, on_update: CASCADE, pk"`
 	SourceSystem   *SourceSystem
 }
 
@@ -44,18 +44,17 @@ type FieldImplementation struct {
 	PreciseUse string
 	Status     ImplementationStatus
 
-	FieldId int64 `sql:"on_delete:RESTRICT, on_update: CASCADE"`
+	FieldId int64 `sql:"on_delete:RESTRICT, on_update: CASCADE, pk"`
 	Field   *Field
 
-	SourceSystemId int32 `sql:"on_delete:RESTRICT, on_update: CASCADE"`
+	SourceSystemId int32 `sql:"on_delete:RESTRICT, on_update: CASCADE, pk"`
 	SourceSystem   *SourceSystem
 }
 
 type RestrictedValue struct {
-	Id          int64
-	Value       string
+	Value       string `sql:",pk"`
 	Description string
 
-	FieldImplementationId int64 `sql:"on_delete:RESTRICT, on_update: CASCADE"`
+	FieldImplementationId int64 `sql:"on_delete:RESTRICT, on_update: CASCADE, pk"`
 	FieldImplementation   *FieldImplementation
 }
